4fb1f38e2604d4c8d8419a311412227340fbedfd
Student Free <l3ra131007@gmail.com>
2025-01-13 15:24:50 +0300

    add button cot and dog

--- src/main/java/com/example/bot/_for_shelter/service/TelegramBot.java
+++ src/main/java/com/example/bot/_for_shelter/service/TelegramBot.java
@@ -1,11 +1,19 @@
 package com.example.bot._for_shelter.service;
 
 import com.example.bot._for_shelter.config.BotConfig;
+import com.vdurmont.emoji.EmojiParser;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -52,6 +60,28 @@ public class TelegramBot extends TelegramLongPollingBot {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(answer);
+
+        InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+        var dogButton = new InlineKeyboardButton();
+        String dogButtonText = EmojiParser.parseToUnicode("Собаки" + " :dog:");
+
+        dogButton.setText(dogButtonText);
+        dogButton.setCallbackData("dog-button");
+
+        var catButton = new InlineKeyboardButton();
+        String catButtonText = EmojiParser.parseToUnicode("Кошки" + " :cat:");
+        catButton.setText(catButtonText);
+        catButton.setCallbackData("cat-button");
+
+        rowInLine.add(dogButton);
+        rowInLine.add(catButton);
+
+        rowsInLine.add(rowInLine);
+
+        markupInLine.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInLine);
 
         try {
             execute(message);

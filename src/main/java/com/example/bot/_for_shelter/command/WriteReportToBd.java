3da3824c933a13dcdc2cec3fe5e4335b597468ac
имя фамилия <Dima>
2025-01-26 17:52:35 +0300

    добавил функцию отправки фотографий

--- src/main/java/com/example/bot/_for_shelter/command/WriteReportToBd.java
+++ src/main/java/com/example/bot/_for_shelter/command/WriteReportToBd.java
@@ -1,16 +1,23 @@
 package com.example.bot._for_shelter.command;
 
+
 import com.example.bot._for_shelter.model.ReportDTO;
 import com.example.bot._for_shelter.service.ReportService;
+
+import com.example.bot._for_shelter.service.TelegramBot;
 import com.example.bot._for_shelter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.*;
+
+import java.util.Comparator;
+import java.util.List;
 
 import static com.example.bot._for_shelter.command.CommandName.WriteReportToBd1;
-import static com.example.bot._for_shelter.command.CommandName.writeContactAtBd;
 
 @Component
 public class WriteReportToBd implements Command {
@@ -18,13 +25,20 @@ public class WriteReportToBd implements Command {
     private final UserService userService;
     private final ReportService reportService;
     private final SendBotMessageService sendBotMessageService;
+    private final TelegramBot telegramBot;
 
-    public WriteReportToBd(UserService userService, ReportService reportService, SendBotMessageService sendBotMessageService) {
+    private final String botToken = "7325728181:AAHXOsha4hsxDQXaSY0k3c54x13dWVwLEu8";
+
+
+    @Autowired
+    @Lazy
+    public WriteReportToBd(UserService userService, ReportService reportService, SendBotMessageService sendBotMessageService, TelegramBot bot, TelegramBot telegramBot) {
         this.userService = userService;
         this.reportService = reportService;
         this.sendBotMessageService = sendBotMessageService;
-    }
 
+        this.telegramBot = telegramBot;
+    }
 
     @Override
     public void execute(Update update) {
@@ -33,10 +47,10 @@ public class WriteReportToBd implements Command {
             userService.changeCondition(chatId, "report");
             SendMessage message = new SendMessage();
             message.setChatId(String.valueOf(chatId));
-            message.setText("Напиши своей текстовый отчет");
+            message.setText("Напиши свой текстовый отчет");
             sendBotMessageService.sendMessageWithKeyboardMarkup(message);
         }
-        if (update.hasMessage()) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
             Long chatId = update.getMessage().getChatId();
             String text = update.getMessage().getText();
             ReportDTO report = new ReportDTO();
@@ -44,11 +58,27 @@ public class WriteReportToBd implements Command {
             report.setChatId(String.valueOf(chatId));
             reportService.addReport(report);
         }
+        if (update.hasMessage() && update.getMessage().hasPhoto()) {
+            long chat_id = update.getMessage().getChatId();
+            List<PhotoSize> photos = update.getMessage().getPhoto();
+            String f_id = photos.stream().max(Comparator.comparing(PhotoSize::getFileSize))
+                    .map(PhotoSize::getFileId)
+                    .orElse("");
+            System.out.println(f_id.length());
+            SendPhoto msg = SendPhoto
+                    .builder()
+                    .chatId(String.valueOf(chat_id))
+                    .photo(new InputFile(f_id))
+                    .build();
 
+            telegramBot.sendPhoto(msg);
+        }
     }
+
 
     @Override
     public boolean isSupport(String command) {
         return command.equals(WriteReportToBd1.getCommandName());
     }
 }
+
--- src/main/java/com/example/bot/_for_shelter/service/TelegramBot.java
+++ src/main/java/com/example/bot/_for_shelter/service/TelegramBot.java
@@ -6,13 +6,19 @@ import com.example.bot._for_shelter.config.BotConfig;
 
 
 import com.example.bot._for_shelter.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
+
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
+import java.util.Comparator;
 import java.util.List;
 
 import static com.example.bot._for_shelter.command.CommandName.writeContactAtBd;
@@ -51,6 +57,10 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasPhoto()) {
+            writeReportToBd.execute(update);
+        }
+
         if (update.hasMessage() && update.getMessage().hasText()) {
             if (update.getMessage().getText().equals("/start")) {
                 startCommand.execute(update);
@@ -61,24 +71,36 @@ public class TelegramBot extends TelegramLongPollingBot {
             String condition = userRepository.findByChatId(chatId).getCondition();
             if (!condition.equals("default")) {
                 writeReportToBd.execute(update);
-            }
 
-            if (update.hasCallbackQuery()) {
-                commandList.stream()
-                        .filter(command -> command.isSupport(update.getCallbackQuery().getData()))
-                        .forEach(command -> {
-                            command.execute(update);
-                        });
-            }
-            if (update.hasMessage() && update.getMessage().hasContact()) {
-                commandList.stream()
-                        .filter(command -> command.isSupport(writeContactAtBd.getCommandName()))
-                        .forEach(command -> {
-                            command.execute(update);
-                        });
             }
         }
+        if (update.hasCallbackQuery()) {
+            commandList.stream()
+                    .filter(command -> command.isSupport(update.getCallbackQuery().getData()))
+                    .forEach(command -> {
+
+                        command.execute(update);
+                    });
+        }
+        if (update.hasMessage() && update.getMessage().hasContact()) {
+            commandList.stream()
+                    .filter(command -> command.isSupport(writeContactAtBd.getCommandName()))
+                    .forEach(command -> {
+                        command.execute(update);
+                    });
+        }
+
     }
+
+    public void sendPhoto(SendPhoto sendPhoto) {
+        try {
+            execute(sendPhoto);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
 
 
